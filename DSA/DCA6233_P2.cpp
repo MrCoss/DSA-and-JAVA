888d63f97519eb8dd1efcfbe067cd6b3dbdc4715
MrCoss <costaspinto312@gmail.com>
2025-07-11 23:43:31 +0530

    Update DCA6233_P2.cpp

--- DSA/DCA6233_P2.cpp
+++ DSA/DCA6233_P2.cpp
@@ -1,29 +1,75 @@
 #include <iostream>
-#include <stack>
 using namespace std;
 
-class StackHandler {
-public:
-    stack<int> s;
+// Node structure for linked list stack
+struct Node {
+    int data;
+    Node* next;
 
-    void load(int arr[], int n) {
-        for (int i = 0; i < n; ++i)
-            s.push(arr[i]);
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+// Stack implementation using linked list
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() : top(nullptr) {}
+
+    void push(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = top;
+        top = newNode;
     }
 
-    void display() {
-        while (!s.empty()) {
-            cout << s.top() << " ";
-            s.pop();
+    void pop() {
+        if (top == nullptr) {
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek() const {
+        if (top == nullptr) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->data;
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+
+    void displayAndClear() {
+        while (!isEmpty()) {
+            cout << peek() << " ";
+            pop();
         }
         cout << endl;
+    }
+
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
     }
 };
 
 int main() {
-    StackHandler sh;
     int values[] = {10, 20, 30, 40};
-    sh.load(values, 4);
-    sh.display();
+    int n = sizeof(values) / sizeof(values[0]);
+
+    Stack s;
+    for (int i = 0; i < n; ++i) {
+        s.push(values[i]);
+    }
+
+    s.displayAndClear();
+
     return 0;
 }
